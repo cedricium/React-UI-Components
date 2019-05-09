98399260596492ede64f79e6e910438c81fd45ee
Cedricium <amaya.cedric@gmail.com>
2019-05-08 18:30:06 -0700

    Flesh out HeaderContainer component and it's children
    Also imported HeaderContainer into the App component so as to display
    the social card.

---
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Header.css';
+
+const HeaderTitle = () => {
+  return (
+    <div className="title-wrapper">
+      <p className="primary">Lambda School</p>
+      <p className="secondary">
+        <span className="username">@LambdaSchool</span>{` `}&bull;{` `}
+        <span className="date">26 Jan</span>
+      </p>
+    </div>
+  )
+}
+
+export default HeaderTitle
