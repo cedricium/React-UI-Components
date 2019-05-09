d1eb604389732401e87f3b4eef2093496b613538
Cedricium <amaya.cedric@gmail.com>
2019-05-08 22:59:42 -0700

    Use smaller bullet point to match design file

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.jsx
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.jsx
@@ -6,7 +6,7 @@ const HeaderTitle = () => {
     <div className="title-wrapper">
       <p className="primary">Lambda School</p>
       <p className="secondary">
-        <span className="username">@LambdaSchool</span>{` `}&bull;{` `}
+        <span className="username">@LambdaSchool</span>{` `}&middot;{` `}
         <span className="date">26 Jan</span>
       </p>
     </div>
