d095f3b8ce063edaf396d636ea0eed6adcada29c
Cedricium <amaya.cedric@gmail.com>
2019-05-08 22:58:03 -0700

    Add react-feather pkg, use in Footer component

--- Project-I-Social-Card/social-card/src/App.js
+++ Project-I-Social-Card/social-card/src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 
 import HeaderContainer from './components/HeaderComponents/HeaderContainer'
 import CardContainer from './components/CardComponents/CardContainer'
+import FooterContainer from './components/FooterComponents/Footer'
 
 const App = () => {
   return (
     <div className="App">
       <HeaderContainer />
       <CardContainer />
+      <FooterContainer />
     </div>
   );
 };
