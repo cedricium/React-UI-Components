2a7b80c6ac937af7f465fbe25a899fc2635e5980
Cedricium <amaya.cedric@gmail.com>
2019-05-08 22:10:46 -0700

    Add click event to entire CardContainer component
    Click event redirects to https://www.reactjs.org per the MVP requirements.

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.jsx
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.jsx
@@ -5,8 +5,11 @@ import CardBanner from './CardBanner'
 import CardContent from './CardContent'
 
 const CardContainer = () => {
+  const handleClick = () => {
+    window.location.assign('https://www.reactjs.org')
+  }
   return (
-    <main className="card-container">
+    <main className="card-container" onClick={handleClick}>
       <div className="card-wrapper">
         <CardBanner />
         <CardContent />
